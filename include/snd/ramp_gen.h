657d76670ea54027c81745f90ebaf562832bd6b7
Colugo <colugomusic@gmail.com>
2020-11-26 01:08:31 +0000

    add rampgen

---
+++ include/snd/ramp_gen.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#pragma warning(push, 0)
+#include <DSP/MLDSPOps.h>
+#pragma warning(pop)
+
+namespace ml {
+
+class RampGen
+{
+public:
+	RampGen(int p) : mCounter(p), mPeriod(p) {}
+	~RampGen() {}
+
+	inline void setPeriod(int p) { mPeriod = p; }
+
+	inline DSPVectorInt operator()()
+	{
+		DSPVectorInt vy;
+		for (int i = 0; i < kFloatsPerDSPVector; ++i)
+		{
+			float fy = 0;
+			if (++mCounter >= mPeriod)
+			{
+				mCounter = 0;
+				fy = 1;
+			}
+			vy[i] = mCounter;
+		}
+		return vy;
+	}
+	int mCounter;
+	int mPeriod;
+};
+
+}
