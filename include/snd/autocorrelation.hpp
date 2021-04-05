4602eea7fa52d6d383651e004002e11dd7f1c276
Colugo <colugomusic@gmail.com>
2021-04-05 12:21:31 +0100

    Create autocorrelation.hpp

---
+++ include/snd/autocorrelation.hpp
@@ -0,0 +1,144 @@
+#pragma once
+
+#include <cassert>
+#include <deque>
+#include <functional>
+#include "misc.h"
+
+namespace snd {
+namespace autocorrelation {
+
+struct AnalysisCallbacks
+{
+	std::function<float(std::uint32_t index)> get_frame;
+	std::function<bool()> should_abort;
+	std::function<void(float)> report_progress;
+};
+
+// specialized autocorrelation which only considers the distances between zero crossings in the analysis.
+// output is an array of estimated wavecycle sizes for each frame
+// if no wavecycle could be found then zero is written
+inline bool analyze(AnalysisCallbacks callbacks, std::uint32_t n, std::uint32_t depth, float* out)
+{
+    if (depth < 4) depth = 4;
+
+    struct ZX
+    {
+        bool init = false;
+        bool up = false;
+        std::uint32_t latest = 0;
+    } zx;
+    
+    struct Crossing
+    {
+        std::uint32_t index;
+        std::int32_t distance;
+        bool up;
+    };
+
+    std::deque<Crossing> crossings;
+
+    const auto find_best_crossing = [](const std::deque<Crossing>& crossings)
+    {
+		constexpr auto AUTO_WIN = 1;
+		
+        auto best = crossings.front().index;
+        auto best_diff = 100;
+        int depth = 2;
+
+        for (;;)
+        {
+            if (depth * 2 > crossings.size())
+            {
+				return best;
+            }
+
+            auto a = crossings.begin();
+            auto b = crossings.begin() + depth;
+
+            auto total_diff = 0;
+
+            for (int i = 0; i < depth; i++, a++, b++)
+            {
+                assert(a->up == b->up);
+
+                total_diff += std::abs(a->distance - b->distance);
+            }
+
+			if (total_diff <= AUTO_WIN)
+			{
+				return (crossings.begin() + depth)->index;
+			}
+
+            if (total_diff < best_diff)
+            {
+                best_diff = total_diff;
+                best = (crossings.begin() + depth)->index;
+            }
+
+            depth++;
+        }
+    };
+
+    auto best_crossing = 0;
+	auto prev_size = 0.0f;
+
+    for (std::uint32_t i = 0; i < n; i++)
+    {
+		if (callbacks.should_abort()) return false;
+
+		const auto value = callbacks.get_frame(i);
+
+        bool crossed = false;
+
+        if (value > 0.0f && (!zx.init || !zx.up))
+        {
+            zx.init = true;
+            zx.up = true;
+            crossed = true;
+        }
+        else if (value < 0.0f && (!zx.init || zx.up))
+        {
+            zx.init = true;
+            zx.up = false;
+            crossed = true;
+        }
+        
+        if (crossed)
+        {
+            Crossing crossing;
+
+            crossing.index = i;
+            crossing.distance = i - zx.latest;
+            crossing.up = zx.up;
+
+            crossings.push_front(crossing);
+
+            if (crossings.size() > depth)
+            {
+                crossings.pop_back();
+            }
+
+            best_crossing = find_best_crossing(crossings);
+
+			const auto size = float(i - best_crossing);
+			const auto distance = i - zx.latest;
+
+			for (std::uint32_t j = 0; j < distance; j++)
+			{
+				const auto x = float(j) / (distance - 1);
+
+				out[j + zx.latest] = lerp(prev_size, size, x);
+			}
+
+			callbacks.report_progress(float(i) / (n - 1));
+
+			prev_size = size;
+            zx.latest = i;
+        }
+    }
+
+	return true;
+}
+
+}}
