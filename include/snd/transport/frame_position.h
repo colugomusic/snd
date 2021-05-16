d09a1d55e64b57697a80a87938c01dc22ca54ce8
Colugo <colugomusic@gmail.com>
2021-05-16 18:07:03 +0100

    Made frame_position header-only

--- include/snd/transport/frame_position.h
+++ include/snd/transport/frame_position.h
@@ -14,7 +14,8 @@ namespace transport {
 template <size_t ROWS>
 struct DSPVectorArrayFramePosition;
 
-// Represents a frame position
+// Represents a factional frame position with very high precision
+// Consists of pos (int32) and fract (float) parts
 class FramePosition
 {
 public:
@@ -196,6 +197,220 @@ template <size_t ROWS>
 DSPVectorArrayFramePosition<ROWS> operator-(std::int32_t a, const DSPVectorArrayFramePosition<ROWS>& b)
 {
 	return DSPVectorArrayFramePosition<ROWS>(a) - b;
+}
+
+bool FramePosition::operator<(const FramePosition& rhs) const
+{
+	if (pos_ < rhs.pos_) return true;
+	if (pos_ == rhs.pos_) return fract_ < rhs.fract_;
+
+	return false;
+}
+
+bool FramePosition::operator<=(const FramePosition& rhs) const
+{
+	if (pos_ < rhs.pos_) return true;
+	if (pos_ == rhs.pos_) return fract_ <= rhs.fract_;
+
+	return false;
+}
+
+bool FramePosition::operator>(const FramePosition& rhs) const
+{
+	if (pos_ > rhs.pos_) return true;
+	if (pos_ == rhs.pos_) return fract_ > rhs.fract_;
+
+	return false;
+}
+
+bool FramePosition::operator>=(const FramePosition& rhs) const
+{
+	if (pos_ > rhs.pos_) return true;
+	if (pos_ == rhs.pos_) return fract_ >= rhs.fract_;
+
+	return false;
+}
+
+bool FramePosition::operator<(std::int32_t rhs) const
+{
+	return pos_ < rhs;
+}
+
+bool FramePosition::operator<=(std::int32_t rhs) const
+{
+	if (pos_ < rhs) return true;
+	if (pos_ == rhs) return fract_ == 0.0f;
+
+	return false;
+}
+
+bool FramePosition::operator>(std::int32_t rhs) const
+{
+	if (pos_ > rhs) return true;
+	if (pos_ == rhs) return fract_ > 0.0f;
+
+	return false;
+}
+bool FramePosition::operator>=(std::int32_t rhs) const
+{
+	if (pos_ > rhs) return true;
+	if (pos_ == rhs) return fract_ >= 0.0f;
+
+	return false;
+}
+
+FramePosition::FramePosition(std::int32_t pos)
+	: pos_(pos)
+	, fract_(0.0f)
+{
+}
+
+FramePosition::FramePosition(std::int32_t pos, float fract)
+	: pos_(pos)
+	, fract_(fract)
+{
+}
+
+FramePosition::FramePosition(float pos)
+{
+	float fractional_part;
+	float int_part;
+
+	fractional_part = std::modf(pos, &int_part);
+
+	pos_ = std::int32_t(int_part);
+	fract_ = fractional_part;
+}
+
+FramePosition& FramePosition::operator=(std::int32_t pos)
+{
+	pos_ = pos;
+	fract_ = 0.0f;
+
+	return *this;
+}
+
+FramePosition& FramePosition::operator+=(const FramePosition& rhs)
+{
+	pos_ += rhs.pos_;
+	fract_ += rhs.fract_;
+
+	if (fract_ >= 1.0f)
+	{
+		fract_ -= 1.0f;
+		pos_++;
+	}
+
+	return *this;
+}
+
+FramePosition& FramePosition::operator-=(const FramePosition& rhs)
+{
+	pos_ -= rhs.pos_;
+	fract_ -= rhs.fract_;
+
+	if (fract_ < 0.0f)
+	{
+		fract_ += 1.0f;
+		pos_--;
+	}
+
+	return *this;
+}
+
+FramePosition& FramePosition::operator+=(std::int32_t rhs)
+{
+	pos_ += rhs;
+
+	return *this;
+}
+
+FramePosition& FramePosition::operator-=(std::int32_t rhs)
+{
+	pos_ -= rhs;
+
+	return *this;
+}
+
+FramePosition& FramePosition::operator+=(float rhs)
+{
+	if (rhs >= 1.0f) return operator+=(FramePosition(rhs));
+
+	fract_ += rhs;
+
+	if (fract_ >= 1.0f)
+	{
+		fract_ -= 1.0f;
+		pos_++;
+	}
+
+	return *this;
+}
+
+FramePosition& FramePosition::operator-=(float rhs)
+{
+	if (rhs >= 1.0f) return operator-=(FramePosition(rhs));
+
+	fract_ -= fract_;
+
+	if (fract_ < 0.0f)
+	{
+		fract_ += 1.0f;
+		pos_--;
+	}
+
+	return *this;
+}
+
+FramePosition::operator float() const
+{
+	return float(pos_) + fract_;
+}
+
+FramePosition::operator double() const
+{
+	return double(pos_) + fract_;
+}
+
+FramePosition::operator std::int32_t() const
+{
+	return pos_;
+}
+
+FramePosition operator+(const FramePosition& a, const FramePosition& b)
+{
+	FramePosition out(a);
+
+	out += b;
+
+	return out;
+}
+
+FramePosition operator-(const FramePosition& a, const FramePosition& b)
+{
+	FramePosition out(a);
+
+	out -= b;
+
+	return out;
+}
+
+FramePosition operator+(const FramePosition& a, std::int32_t b)
+{
+	FramePosition out(a);
+
+	out += b;
+
+	return out;
+}
+
+FramePosition operator-(const FramePosition& a, std::int32_t b)
+{
+	FramePosition out(a);
+
+	out -= b;
+
+	return out;
 }
 
 }}
