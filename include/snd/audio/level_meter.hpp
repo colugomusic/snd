3d2aa6ec1619c1e7d94e40df2359b673aa4d2ec2
Colugo <colugomusic@gmail.com>
2021-05-25 18:09:45 +0100

    add LevelMeter::process_mono

--- include/snd/audio/level_meter.hpp
+++ include/snd/audio/level_meter.hpp
@@ -21,10 +21,13 @@ class LevelMeter
 	std::array<std::atomic<float>, ROWS> current_value_;
 	std::array<std::atomic<float>, ROWS> peak_;
 
+	void process_channel(int index, const ml::DSPVector& in);
+
 public:
 
 	LevelMeter(int time = kFloatsPerDSPVector * 64);
 
+	void process_mono(const ml::DSPVector& in);
 	void operator()(const ml::DSPVectorArray<ROWS>& in);
 	float operator[](std::size_t index) const;
 };
@@ -39,31 +42,43 @@ LevelMeter<ROWS>::LevelMeter(int time)
 }
 
 template <size_t ROWS>
+void LevelMeter<ROWS>::process_channel(int index, const ml::DSPVector& in)
+{
+	const auto low = std::abs(ml::min(in));
+	const auto high = std::abs(ml::max(in));
+
+	auto peak = ml::max(low, high);
+
+	if (peak > peak_[index])
+	{
+		glide_[index].setValue(peak);
+	}
+
+	const auto glide = glide_[index](peak);
+
+	auto current_value = ml::max(glide);
+
+	peak = ml::max(glide);
+
+	if (current_value < 0.000001f) current_value = 0.0f;
+	if (peak < 0.000001f) peak = 0.0f;
+
+	current_value_[index] = current_value;
+	peak_[index] = peak;
+}
+
+template <size_t ROWS>
+void LevelMeter<ROWS>::process_mono(const ml::DSPVector& in)
+{
+	process_channel(0, in);
+}
+
+template <size_t ROWS>
 void LevelMeter<ROWS>::operator()(const ml::DSPVectorArray<ROWS>& in)
 {
 	for (int i = 0; i < ROWS; i++)
 	{
-		const auto low = std::abs(ml::min(in.constRow(i)));
-		const auto high = std::abs(ml::max(in.constRow(i)));
-
-		auto peak = ml::max(low, high);
-
-		if (peak > peak_[i])
-		{
-			glide_[i].setValue(peak);
-		}
-
-		const auto glide = glide_[i](peak);
-
-		auto current_value = ml::max(glide);
-
-		peak = ml::max(glide);
-
-		if (current_value < 0.000001f) current_value = 0.0f;
-		if (peak < 0.000001f) peak = 0.0f;
-
-		current_value_[i] = current_value;
-		peak_[i] = peak;
+		process_channel(i, in.constRow(i));
 	}
 }
 
