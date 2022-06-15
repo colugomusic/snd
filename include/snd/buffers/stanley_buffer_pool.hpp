ea79429f39f5b7cf87fcb0e3ba88e4b045ae4150
Colugo <68328892+colugomusic@users.noreply.github.com>
2022-06-15 16:57:19 +0100

    Create stanley_buffer_pool.hpp

---
+++ include/snd/buffers/stanley_buffer_pool.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <cassert>
+#include <deque>
+#include <unordered_map>
+#include "stanley_buffer.hpp"
+
+namespace snd {
+
+template <size_t SIZE = STANLEY_BUFFER_DEFAULT_SIZE, class Allocator = std::allocator<float>>
+class StanleyBufferPool
+{
+public:
+
+	using buffer_t = StanleyBuffer<SIZE, Allocator>;
+	using row_t = typename buffer_t::row_t;
+
+	auto acquire(row_t row_count) -> std::unique_ptr<buffer_t>;
+	auto release(std::unique_ptr<buffer_t> sbuffer) -> void;
+
+private:
+
+	std::unordered_map<row_t, std::deque<std::unique_ptr<buffer_t>>> buffers_;
+};
+
+template <size_t SIZE, class Allocator>
+auto StanleyBufferPool<SIZE, Allocator>::acquire(row_t row_count) -> std::unique_ptr<buffer_t>
+{
+	if (buffers_[row_count].empty())
+	{
+		return std::make_unique<buffer_t>(row_count);
+	}
+
+	auto out{ std::move(buffers_[row_count].front()) };
+
+	buffers_[row_count].pop_front();
+
+	return out;
+}
+
+template <size_t SIZE, class Allocator>
+auto StanleyBufferPool<SIZE, Allocator>::release(std::unique_ptr<buffer_t> buffer) -> void
+{
+	buffer->release();
+	buffers_[buffer->get_row_count()].push_back(std::move(buffer));
+}
+
+} // snd
